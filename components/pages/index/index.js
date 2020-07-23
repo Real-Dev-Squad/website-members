b64646aafe847eab99845ba3290e4c04da39abd6
invalidtoken <codeit.sahil98@gmail.com>
2020-07-23 23:47:02 +0530

    pushing exports to the end of the file

--- components/pages/index/index.js
+++ components/pages/index/index.js
@@ -1,7 +1,13 @@
 import React from "react";
+import classNames from "./index.scss";
 
 const HomePage = () => {
-  return <h1>Our Members</h1>;
+  return (
+    <div className={classNames.container}>
+      <img className={classNames.img} src="/images/Real-Dev-Squad@1x.png" />
+      <h1 className={classNames.heading}>Real Dev Squad Members</h1>
+    </div>
+  );
 };
 
 export default HomePage;
