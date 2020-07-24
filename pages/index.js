a9026920de79b7e66cfe6ae186ef4f53226c8a9e
invalidtoken <codeit.sahil98@gmail.com>
2020-07-24 23:37:01 +0530

    installing eslint

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
-import React from "react";
-import "../styles/global-styles.scss";
-import Head from "next/head";
-import HomePage from "../components/pages/index";
+import React from 'react';
+import '../styles/global-styles.scss';
+import Head from 'next/head';
+import HomePage from '../components/pages/index';
 
 const Index = () => {
   return (
@@ -9,11 +9,7 @@ const Index = () => {
       <Head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link
-          rel="shortcut icon"
-          href="/icons/favicon.ico"
-          type="image/x-icon"
-        />
+        <link rel="shortcut icon" href="/icons/favicon.ico" type="image/x-icon" />
         <link rel="icon" href="/icons/favicon.ico" type="image/x-icon" />
         <title>Members | Real Dev Squad</title>
         <link
