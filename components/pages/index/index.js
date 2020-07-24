28ead4caa1cefa5a7272d78410ababdc0b1b3e23
invalidtoken <codeit.sahil98@gmail.com>
2020-07-25 00:57:52 +0530

    formatting stylesheets in the wrong way

--- components/pages/index/index.js
+++ components/pages/index/index.js
@@ -5,6 +5,7 @@ const HomePage = () => {
   return (
     <div className={classNames.container}>
       <img className={classNames.img} src="/images/Real-Dev-Squad@1x.png" alt="real-dev squad" />
+
       <h1 className={classNames.heading}>Real Dev Squad Members</h1>
     </div>
   );
