c02706882183252bcb4020e43915b812a7f79513
invalidtoken <codeit.sahil98@gmail.com>
2020-07-25 01:04:30 +0530

    changing eslintrc file

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,14 +25,6 @@ module.exports = {
   ],
   rules: {
     'prettier/prettier': ['error', {}, { usePrettierrc: true }],
-    'react/react-in-jsx-scope': 'off',
-    'jsx-a11y/anchor-is-valid': [
-      'error',
-      {
-        components: ['Link'],
-        specialLink: ['hrefLeft', 'hrefRight'],
-        aspects: ['invalidHref', 'preferButton']
-      }
-    ]
+    'react/react-in-jsx-scope': 'off'
   }
 };
