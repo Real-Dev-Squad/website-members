ff78942d781514b53e95ff61747eecec3f0aa593
invalidtoken <codeit.sahil98@gmail.com>
2020-07-25 22:41:01 +0530

    adding _document for meta and link tags

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,8 @@
-import Head from 'next/head';
 import HomePage from '../components/pages/index';
 
 const Index = () => {
   return (
     <div>
-      <Head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="shortcut icon" href="/icons/favicon.ico" type="image/x-icon" />
-        <link rel="icon" href="/icons/favicon.ico" type="image/x-icon" />
-        <title>Members | Real Dev Squad</title>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;400&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
       <HomePage />
     </div>
   );
