2ed680ac6850b2d6e50f511b4bf7210c375274f4
invalidtoken <codeit.sahil98@gmail.com>
2020-07-25 22:41:01 +0530

    adding _document for meta and link tags

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,11 @@ import '../styles/global-styles.scss';
 
 const MyApp = (props) => {
   const { Component, pageProps } = props;
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <Component {...pageProps} />
+    </div>
+  );
 };
 
 export default MyApp;
