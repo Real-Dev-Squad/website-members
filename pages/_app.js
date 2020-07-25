fafd801458779dee35f9cdd4fa612cdca9c7bc78
invalidtoken <codeit.sahil98@gmail.com>
2020-07-26 00:52:46 +0530

    adding prop-types in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,19 @@
 import '../styles/global-styles.scss';
+import PropTypes from 'prop-types';
 
 const MyApp = (props) => {
+  // eslint-disable-next-line react/prop-types
   const { Component, pageProps } = props;
   return (
     <div>
       <Component {...pageProps} />
     </div>
   );
+};
+
+MyApp.prototype = {
+  Component: PropTypes.element,
+  pageProps: PropTypes.any
 };
 
 export default MyApp;
