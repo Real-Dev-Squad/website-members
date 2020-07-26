0ee200a2ecdd947119cfa6d091153c0122eec8ee
invalidtoken <codeit.sahil98@gmail.com>
2020-07-26 17:22:44 +0530

    adding useEffect

--- components/pages/index/index.js
+++ components/pages/index/index.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Head from 'next/head';
 import classNames from './home-page.module.scss';
 
 const HomePage = () => {
+  useEffect(() => {}, []);
+
   return (
     <div className={classNames.container}>
+      <Head>
+        <title>Members | Real Dev Squad</title>
+      </Head>
       <img className={classNames.img} src="/images/Real-Dev-Squad@1x.png" alt="real-dev squad" />
-
       <h1 className={classNames.heading}>Real Dev Squad Members</h1>
     </div>
   );
