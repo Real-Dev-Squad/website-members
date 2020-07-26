b349243266f7dc24361a183accc7620dd4862724
invalidtoken <codeit.sahil98@gmail.com>
2020-07-26 19:22:27 +0530

    passing rdsId

--- components/members-list/index.js
+++ components/members-list/index.js
@@ -1,11 +1,13 @@
+import MemberListItem from 'components/member-list-item';
 import Loader from 'components/loader';
+
 import useFetchHook from 'custom-hooks/useFetchHook';
+import classNames from './member-list.module.scss';
 
 const URL = 'https://raw.githubusercontent.com/Real-Dev-Squad/website-static/main/ids/mapping.json';
 
 const MembersList = () => {
   let { loading, error, data } = useFetchHook(URL);
-  console.log(loading, error, data);
 
   if (loading) {
     return <Loader />;
@@ -16,7 +18,17 @@ const MembersList = () => {
   }
 
   if (data) {
-    return <div>data</div>;
+    let rdsIds = Object.keys(data);
+    console.log(rdsIds);
+    return (
+      <div className={classNames.container}>
+        {rdsIds.map((rdsId) => (
+          <div key={rdsId}>
+            <MemberListItem rdsId={rdsId} />
+          </div>
+        ))}
+      </div>
+    );
   }
 
   return null;
