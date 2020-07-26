891c05383e4b5c4a4abdd94432404772354c563b
invalidtoken <codeit.sahil98@gmail.com>
2020-07-26 21:59:09 +0530

    changin charset to charSet

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <meta charset="UTF-8" />
+          <meta charSet="UTF-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <link rel="shortcut icon" href="/icons/favicon.ico" type="image/x-icon" />
           <link rel="icon" href="/icons/favicon.ico" type="image/x-icon" />
