1636893bf4d7a60dd4cdb978c2fb3acadc846aee
invalidtoken <codeit.sahil98@gmail.com>
2020-07-26 19:22:27 +0530

    passing rdsId

--- components/member-list-item/index.js
+++ components/member-list-item/index.js
@@ -1,4 +1,13 @@
+import { string } from 'prop-types';
+
 // Render a member's information
-const PreviewMember = () => {};
+const PreviewMember = (props) => {
+  const { rdsId } = props;
+  return <div>{rdsId}</div>;
+};
+
+PreviewMember.propTypes = {
+  rdsId: string
+};
 
 export default PreviewMember;
