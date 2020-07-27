e4648e96b3f4ab86b6cbffe48321a8d31ad6f111
Tejas Beri <tejas.beri@transport.nsw.gov.au>
2020-07-27 20:13:23 +1000

    Refactoring: Creare common component for social media icons
    - Created common component
    - refactored member-ietms

--- components/member-list-item/index.js
+++ components/member-list-item/index.js
@@ -1,18 +1,16 @@
-import { string } from 'prop-types';
-import Link from 'next/link';
 import { getDataURL, getImgURL } from 'helper-functions/urls';
+import { useEffect, useRef } from 'react';
+
+import Link from 'next/link';
+import SocialMediaIcon from '../social-media-icon';
 import classNames from './member-list-item.module.scss';
-import { useRef, useEffect } from 'react';
+import { string } from 'prop-types';
 import useFetch from 'custom-hooks/useFetch';
 
 const PreviewMember = (props) => {
   const { rdsId } = props;
   const { data } = useFetch(getDataURL(rdsId));
   const imgRef = useRef();
-
-  const onClick = (e) => {
-    e.stopPropagation();
-  };
 
   useEffect(() => {
     if (imgRef && imgRef.current) {
@@ -34,43 +32,16 @@ const PreviewMember = (props) => {
       {data && (
         <div className={classNames.iconsContainer}>
           {data['twitter_id'] && (
-            <a
-              onClick={onClick}
-              className={classNames.iconContainer}
-              target="_blank"
-              rel="noreferrer"
-              href={`//twitter.com/${data['twitter_id']}`}>
-              <img className={classNames.icon} alt="twitter icon" src="/icons/twitter.png" />
-            </a>
+            <SocialMediaIcon id={data.twitter_id} type="twitter"></SocialMediaIcon>
           )}
           {data['github_id'] && (
-            <a
-              onClick={onClick}
-              target="_blank"
-              rel="noreferrer"
-              href={`//github.com/${data['github_id']}`}
-              className={classNames.iconContainer}>
-              <img className={classNames.icon} alt="github icon" src="/icons/github.png" />
-            </a>
+            <SocialMediaIcon id={data.github_id} type="github"></SocialMediaIcon>
           )}
           {data['linkedin_id'] && (
-            <a
-              onClick={onClick}
-              target="_blank"
-              rel="noreferrer"
-              href={`//linkedin.com/in/${data['linkedin_id']}`}
-              className={classNames.iconContainer}>
-              <img className={classNames.icon} alt="github icon" src="/icons/linkedin.png" />
-            </a>
+            <SocialMediaIcon id={data.linkedin_id} type="linkedIn"></SocialMediaIcon>
           )}
           {data['instagram_id'] && (
-            <a
-              target="_blank"
-              rel="noreferrer"
-              href={`//instagram.com/${data['instagram_id']}`}
-              className={classNames.iconContainer}>
-              <img className={classNames.icon} alt="github icon" src="/icons/instagram.png" />
-            </a>
+            <SocialMediaIcon id={data.instagram_id} type="instagram"></SocialMediaIcon>
           )}
         </div>
       )}
