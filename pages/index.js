97eab02e7143d7394cf15ec3c2688ed51a292c2b
ashish <ashishmangla1991@gmail.com>
2020-07-31 15:48:00 +0530

    remove extra utf-8

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,6 @@ const Index = () => {
   return (
     <div>
       <Head>
-        <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link
           rel="shortcut icon"
