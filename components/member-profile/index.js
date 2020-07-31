53d26b0e63f103cc9278e96f2df3b6453f47a0f4
Nikhil Bhandarkar <nikhil.bhandarkar@waycool.in>
2020-07-31 16:56:41 +0530

    Add Layout Component

--- components/member-profile/index.js
+++ components/member-profile/index.js
@@ -2,18 +2,28 @@ import React from 'react';
 import { getDataURL, getImgURL } from 'helper-functions/urls';
 import classNames from './member-profile.module.scss';
 import useFetch from 'custom-hooks/useFetch';
+import PropTypes from 'prop-types';
 
 const Profile = (props) => {
-  const imgLink = getImgURL(props.id);
-  const { data } = useFetch(getDataURL(props.id));
+  const { id } = props;
+  const imgLink = getImgURL(id);
+  const { data } = useFetch(getDataURL(id));
   return (
     <>
-      <img src={imgLink} className={classNames.profilePic} />
+      <img src={imgLink} alt="" className={classNames.profilePic} />
       <div className={classNames.memberName}>
-        {data ? `${data['first_name']} ${data['last_name']} ` : `${props.id}`}
+        {data ? `${data['first_name']} ${data['last_name']} ` : `${id}`}
       </div>
     </>
   );
+};
+
+Profile.propTypes = {
+  id: PropTypes.string
+};
+
+Profile.defaultProps = {
+  id: ''
 };
 
 export default Profile;
