2ddf4061cbcade2a08cf7a0ab6733242affa1528
Sumit <sumitdhanania94@gmail.com>
2020-08-10 17:32:08 +0530

    Fixed the issues as per comments

--- pages/members/[id]/index.js
+++ pages/members/[id]/index.js
@@ -3,26 +3,43 @@ import PropTypes from 'prop-types';
 import fetch from 'cross-fetch';
 import React from 'react';
 import Profile from 'components/member-profile';
+import NotFound from 'components/404page';
 
-const MemberProfile = ({ imageLink, data }) => {
+const MemberProfile = ({ imageLink, data, errorMessage }) => {
+  if (errorMessage) {
+    return <NotFound />;
+  }
   return <Profile imageLink={imageLink} membersData={data} />;
 };
 
 export async function getServerSideProps(context) {
-  const imgLink = getImgURL(context.params.id);
-  const jsonUrl = getDataURL(context.params.id);
-  const res = await fetch(jsonUrl);
-  const returnData = await res.json();
+  const { params } = context;
+  const imgLink = getImgURL(params.id);
+  const jsonUrl = getDataURL(params.id);
 
-  return { props: { imageLink: imgLink, data: returnData } };
+  try {
+    const res = await fetch(jsonUrl);
+    if (res.status !== 200) {
+      throw new Error('Invalid Username');
+    }
+    const returnData = await res.json();
+
+    return { props: { imageLink: imgLink, data: returnData } };
+  } catch (e) {
+    return { props: { errorMessage: e.message } };
+  }
 }
 
 MemberProfile.propTypes = {
-  imageLink: PropTypes.string
+  imageLink: PropTypes.string,
+  data: PropTypes.object,
+  errorMessage: PropTypes.string
 };
 
-MemberProfile.propTypes = {
-  data: PropTypes.object
+MemberProfile.defaultProps = {
+  imageLink: '',
+  data: {},
+  errorMessage: ''
 };
 
 export default MemberProfile;
