c59a846e8d6b4ee93c6ac680fbf0c103f8783a3e
Sumit <sumitdhanania94@gmail.com>
2020-08-11 02:04:27 +0530

    Improved the error msg & code improvements

--- components/member-profile/index.js
+++ components/member-profile/index.js
@@ -1,14 +1,16 @@
 import PropTypes from 'prop-types';
-import React from 'react';
 import classNames from './member-profile.module.scss';
 
 const Profile = (props) => {
-  const { membersData, imageLink } = props;
-  const memberName = `${membersData.first_name || '--'} ${membersData.last_name || '--'}`;
+  const {
+    membersData: { first_name, last_name },
+    imageLink
+  } = props;
+  const memberName = `${first_name || ''} ${last_name || ''}`;
   return (
     <>
       <img src={imageLink} className={classNames.profilePic} alt="profile" />
-      <div className={classNames.memberName}>{memberName}</div>
+      <div className={classNames.memberName}>{memberName.trim() || '--'}</div>
     </>
   );
 };
