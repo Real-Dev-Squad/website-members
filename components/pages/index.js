39d187e8c2d7d14deef26b462984ab679704cfb4
Sumit Dhanania <sumitdhanania94@gmail.com>
2020-08-16 12:41:58 +0530

    Converted homepage into SSR

--- components/pages/index.js
+++ components/pages/index.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import PropTypes from 'prop-types';
 import MemberList from 'components/members-list';
 import classNames from './home-page.module.scss';
 
-const HomePage = () => {
+const HomePage = ({ membersKey }) => {
   return (
     <div className={classNames.container}>
       <img className={classNames.img} src="/images/Real-Dev-Squad@1x.png" alt="real-dev squad" />
       <h1 className={classNames.heading}>Real Dev Squad Members</h1>
-      <MemberList />
+      <MemberList membersKey={membersKey} />
     </div>
   );
+};
+
+HomePage.propTypes = {
+  membersKey: PropTypes.object
+};
+
+HomePage.defaultProps = {
+  membersKey: {}
 };
 
 export default HomePage;
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,47 @@
-import HomePage from '../components/pages/index';
+import PropTypes from 'prop-types';
+import fetch from 'cross-fetch';
+import HomePage from '../components/pages';
 import Layout from '../components/layout';
+import NotFound from 'components/not-found-page';
 
-const Index = () => {
+const Index = ({ data, errorMsg }) => {
+  if (errorMsg) {
+    return <NotFound errorMsg={errorMsg} />;
+  }
+
   return (
     <Layout title={'Members | Real Dev Squad'}>
-      <HomePage />
+      <HomePage membersKey={data} />
     </Layout>
   );
+};
+
+export async function getServerSideProps() {
+  const URL =
+    'https://raw.githubusercontent.com/Real-Dev-Squad/website-static/main/ids/mapping.json';
+
+  try {
+    const res = await fetch(URL);
+    if (res.status !== 200) {
+      throw new Error(
+        'There was some issues fetching the members, Please try again after some time'
+      );
+    }
+    const data = await res.json();
+    return { props: { data } };
+  } catch (e) {
+    return { props: { errorMsg: e.message } };
+  }
+}
+
+Index.propTypes = {
+  data: PropTypes.object,
+  errorMsg: PropTypes.string
+};
+
+Index.defaultProps = {
+  data: {},
+  errorMsg: ''
 };
 
 export default Index;
