5e2a57d1246b248af78308667750eeadac57967d
Sumit Dhanania <sumitdhanania94@gmail.com>
2020-08-30 22:34:00 +0530

    Added the logic to fetch member details Also in SSR

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import { getDataURL, getImgURL } from 'helper-functions/urls';
 import PropTypes from 'prop-types';
 import fetch from 'cross-fetch';
 import HomePage from '../components/pages';
@@ -5,11 +6,13 @@ import Layout from '../components/layout';
 import NotFound from 'components/not-found-page';
 
 const Index = ({ data, errorMsg }) => {
+  // console.log(data);
+  // return <p>Hello</p>;
   let loadComponent = '';
   if (errorMsg) {
     loadComponent = <NotFound errorMsg={errorMsg} />;
   } else {
-    loadComponent = <HomePage membersKey={data} />;
+    loadComponent = <HomePage membersArr={data} />;
   }
 
   return <Layout title={'Members | Real Dev Squad'}>{loadComponent}</Layout>;
@@ -19,6 +22,11 @@ export async function getServerSideProps() {
   const URL =
     'https://raw.githubusercontent.com/Real-Dev-Squad/website-static/main/ids/mapping.json';
 
+  let membersArr = [];
+  let memberDetailsUrl = '';
+  let resp1 = '';
+  let jsonObj = '';
+
   try {
     const res = await fetch(URL);
     if (res.status !== 200) {
@@ -26,7 +34,20 @@ export async function getServerSideProps() {
         'There was some issues fetching the members, Please try again after some time'
       );
     }
-    const data = await res.json();
+    let data = await res.json();
+
+    for (const rdsid in data) {
+      memberDetailsUrl = getDataURL(rdsid);
+      resp1 = await fetch(memberDetailsUrl);
+      jsonObj = await resp1.json();
+      membersArr.push({
+        ...jsonObj,
+        img_url: getImgURL(rdsid)
+      });
+    }
+
+    data = membersArr;
+
     return { props: { data } };
   } catch (e) {
     return { props: { errorMsg: e.message } };
@@ -34,12 +55,12 @@ export async function getServerSideProps() {
 }
 
 Index.propTypes = {
-  data: PropTypes.object,
+  data: PropTypes.array,
   errorMsg: PropTypes.string
 };
 
 Index.defaultProps = {
-  data: {},
+  data: [],
   errorMsg: ''
 };
 
