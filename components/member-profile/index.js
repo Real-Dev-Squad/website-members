089383ae24a762fdaff7a6e9044d85922c0719db
Tejas Beri <tejas.beri@transport.nsw.gov.au>
2020-11-23 22:24:23 +1100

    open links in a new tab

--- components/member-profile/index.js
+++ components/member-profile/index.js
@@ -1,7 +1,6 @@
-import React from 'react';
 import PropTypes from 'prop-types';
+import React from 'react';
 import SocialMediaIcon from '../social-media-icon';
-
 import classNames from './member-profile.module.scss';
 
 const Profile = (props) => {
@@ -99,6 +98,8 @@ const Profile = (props) => {
                 <path d="M7.177 3.073L9.573.677A.25.25 0 0110 .854v4.792a.25.25 0 01-.427.177L7.177 3.427a.25.25 0 010-.354zM3.75 2.5a.75.75 0 100 1.5.75.75 0 000-1.5zm-2.25.75a2.25 2.25 0 113 2.122v5.256a2.251 2.251 0 11-1.5 0V5.372A2.25 2.25 0 011.5 3.25zM11 2.5h-1V4h1a1 1 0 011 1v5.628a2.251 2.251 0 101.5 0V5A2.5 2.5 0 0011 2.5zm1 10.25a.75.75 0 111.5 0 .75.75 0 01-1.5 0zM3.75 12a.75.75 0 100 1.5.75.75 0 000-1.5z" />
               </svg>
               <a
+                target="_blank"
+                rel="noreferrer"
                 href="https://github.com/Real-Dev-Squad/website-backend/pull/26"
                 className={classNames.title}>
                 User details
@@ -118,11 +119,13 @@ const Profile = (props) => {
                 height="16"
                 aria-hidden="true">
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M7.177 3.073L9.573.677A.25.25 0 0110 .854v4.792a.25.25 0 01-.427.177L7.177 3.427a.25.25 0 010-.354zM3.75 2.5a.75.75 0 100 1.5.75.75 0 000-1.5zm-2.25.75a2.25 2.25 0 113 2.122v5.256a2.251 2.251 0 11-1.5 0V5.372A2.25 2.25 0 011.5 3.25zM11 2.5h-1V4h1a1 1 0 011 1v5.628a2.251 2.251 0 101.5 0V5A2.5 2.5 0 0011 2.5zm1 10.25a.75.75 0 111.5 0 .75.75 0 01-1.5 0zM3.75 12a.75.75 0 100 1.5.75.75 0 000-1.5z"
                 />
               </svg>
               <a
+                target="_blank"
+                rel="noreferrer"
                 href="https://github.com/Real-Dev-Squad/website-backend/pull/26"
                 className={classNames.title}>
                 {' '}
@@ -143,10 +146,12 @@ const Profile = (props) => {
                 height="16"
                 aria-hidden="true">
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M7.177 3.073L9.573.677A.25.25 0 0110 .854v4.792a.25.25 0 01-.427.177L7.177 3.427a.25.25 0 010-.354zM3.75 2.5a.75.75 0 100 1.5.75.75 0 000-1.5zm-2.25.75a2.25 2.25 0 113 2.122v5.256a2.251 2.251 0 11-1.5 0V5.372A2.25 2.25 0 011.5 3.25zM11 2.5h-1V4h1a1 1 0 011 1v5.628a2.251 2.251 0 101.5 0V5A2.5 2.5 0 0011 2.5zm1 10.25a.75.75 0 111.5 0 .75.75 0 01-1.5 0zM3.75 12a.75.75 0 100 1.5.75.75 0 000-1.5z"></path>
               </svg>
               <a
+                target="_blank"
+                rel="noreferrer"
                 href="https://github.com/Real-Dev-Squad/website-backend/pull/26"
                 className={classNames.title}>
                 {' '}
