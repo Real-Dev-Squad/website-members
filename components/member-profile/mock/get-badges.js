3a3fc84be5116d804d929753224839597b5deb53
ankushdharkar <ankushdharkar@gmail.com>
2020-12-23 16:23:06 -0800

    Basic badges setup for members #88

---
+++ components/member-profile/mock/get-badges.js
@@ -0,0 +1,20 @@
+import badgesData from './data.json';
+
+const imgBaseURL = badgesData['images']['baseURL'];
+
+export default function (memberId) {
+  let memberBadges = badgesData[memberId];
+
+  if (!memberBadges) {
+    memberBadges = badgesData['default'];
+  }
+
+  memberBadges = memberBadges.map((badge) => {
+    return {
+      title: badge.title,
+      img: imgBaseURL + badgesData['images'][badge.imgKey]
+    };
+  });
+
+  return memberBadges;
+}
