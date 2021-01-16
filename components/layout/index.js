ce549ecfe4c91d9809016594adb94fdc787a89ee
RohitKuwar <70262783+RohitKuwar@users.noreply.github.com>
2021-01-16 13:41:52 +0530

    Added SideDrawer

--- components/layout/index.js
+++ components/layout/index.js
@@ -1,15 +1,24 @@
 import Head from 'next/head';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import Footer from 'components/footer';
 import Navbar from 'components/navbar';
+import SideDrawer from 'components/navbar/SideDrawer/SideDrawer';
 const Layout = ({ children, title, description }) => {
+  const [showSideDrawer, setShowSideDrawer] = useState(false);
+
+  const sideDrawerToggleHandler = () => {
+    setShowSideDrawer((prevState) => !prevState);
+  };
+
   return (
     <div>
       <Head>
         <title>{title}</title>
         <meta name="description" content={description} />
       </Head>
-      <Navbar />
+      <Navbar drawerToggleClicked={sideDrawerToggleHandler} />
+      <SideDrawer open={showSideDrawer} />
       <div className="container">
         <div>{children}</div>
       </div>
