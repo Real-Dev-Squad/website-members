bdbb3f94c0db4e93bf68123efe30ee3ab3958f48
Sumit Dhanania <sumitdhanania94@gmail.com>
2021-01-19 17:44:50 +0530

    Fix members profile redirect url & remove vieport meta tag from _document.js

--- components/layout/index.js
+++ components/layout/index.js
@@ -8,6 +8,7 @@ const Layout = ({ children, title, description }) => {
       <Head>
         <title>{title}</title>
         <meta name="description" content={description} />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <Navbar />
       <div className="container">
