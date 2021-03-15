1652ebadd28c650e7c94db6036c068bea4187ce2
Deipayan Dash <deipayan.dash8@gmail.com>
2021-03-15 17:21:23 +0530

    Added sample API fetch() and fixed naming

--- components/modal/shared/Header/header.component.js
+++ components/modal/shared/Header/header.component.js
@@ -2,7 +2,7 @@ import React from 'react';
 import classNames from './header.component.module.scss';
 
 function Header() {
-  return <h1 className={classNames.headerQuote}>Send Your Interest</h1>;
+  return <h1 className={classNames.modalheader}>Send Your Interest</h1>;
 }
 
 export default Header;
