69b29b3ab133d50c000f459046af5e41ceb6c4f0
Sumit Dhanania <sumitdhanania94@gmail.com>
2021-07-02 18:04:30 +0530

    Update jest config

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,8 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleNameMapper: {
     '\\.(css|scss|sass|less)$': 'identity-obj-proxy',
-    '^@components(.*)$': '<rootDir>/components$1'
+    '^@components(.*)$': '<rootDir>/components$1',
+    '^@pages(.*)$': '<rootDir>/pages$1',
+    '^@styles(.*)$': '<rootDir>/styles$1'
   }
 };
