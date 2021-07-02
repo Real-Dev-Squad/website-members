a96c451b9eb9829423404ff104f7c2e9140fdef5
akshay shinde <akshayshinde7289@gmail.com>
2021-07-03 00:14:05 +0530

    made some changes as per requested

--- components/member-card/showskills.js
+++ components/member-card/showskills.js
@@ -1,20 +1,36 @@
-import { skills } from './skills';
+import { skills_data } from './skills';
 import classNames from './card.module.scss';
+import PropTypes from 'prop-types';
 
-const Showskills = () => {
-  //   skills.map(skill => console.log(skill));
+const Showskills = ({ show }) => {
   return (
     <div className={classNames.showskills}>
-      {skills.map((skill) => {
-        // eslint-disable-next-line react/jsx-key
+      {skills_data.topskills.map((skill) => {
         return (
-          // eslint-disable-next-line react/jsx-key
-          <p style={{ background: `${skill.color}` }} className={classNames.techskills}>
+          <p
+            style={{ background: `${skill.background}` }}
+            className={classNames.techskills}
+            key={skill.color}>
             {skill.tech}
           </p>
         );
       })}
+      {show &&
+        skills_data.skills.map((skill) => {
+          return (
+            <p
+              style={{ background: `${skill.background}` }}
+              className={classNames.techskills}
+              key={skill.color}>
+              {skill.tech}
+            </p>
+          );
+        })}
     </div>
   );
+};
+
+Showskills.propTypes = {
+  show: PropTypes.bool.isRequired
 };
 export default Showskills;
