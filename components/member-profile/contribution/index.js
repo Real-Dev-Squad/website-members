81b9d829e10ed53e26e889e84ad466aa5c771fad
Lakshay Manchanda <lakshaymanchanda73@gmail.com>
2021-08-05 14:47:31 +0530

    Redirected to feature URL

--- components/member-profile/contribution/index.js
+++ components/member-profile/contribution/index.js
@@ -68,7 +68,9 @@ const Contribution = ({ contribution, fullName, imageLink, devUser }) => {
         </div>
       </div>
       <div className={classNames.featureLink}>
-        <a href={featureUrl || prList[0]['url']} target="_blank"> Check out this feature in action</a>
+        <a href={featureUrl || prList[0]['url']} target="_blank" rel="noreferrer">
+          Check out this feature in action
+        </a>
       </div>
       <hr className={classNames.line}></hr>
     </div>
