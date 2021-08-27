d242fb5d9123f5fcc3b26c4cb45e0ae335949622
takshch <takshch089@gmail.com>
2021-08-28 00:42:32 +0530

    Adds comments for percentageofDaysRemaining helper function

--- helper-functions/taskProgress.js
+++ helper-functions/taskProgress.js
@@ -1,5 +1,11 @@
 import { timeWas } from 'helper-functions/time-was';
 
+/**
+ * @param {Number} startedOn
+ * @param {Number} endsOn
+ * @param {String} completedDate
+ * @returns {String} percentageofDaysRemaining
+ */
 function percentageofDaysRemaining(startedOn, endsOn, completedDate) {
   const startDate = timeWas(startedOn * 1000, true, endsOn * 1000);
   const endDate = timeWas(endsOn * 1000, true, startDate * 1000);
