75735be47b2190fc895fbae7f6d8cd4ef0bf5126
Sumit Dhanania <sumitdhanania94@gmail.com>
2021-09-02 15:26:23 +0530

    change modal props

--- src/components/UI/modal/index.jsx
+++ src/components/UI/modal/index.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import classes from 'components/UI/modal/modal.module.scss';
+import styles from 'components/UI/modal/modal.module.scss';
 import Backdrop from 'components/UI/backdrop';
 
-const Modal = ({ show, modalClosed, children }) => (
+const Modal = ({ show, closeModal, children }) => (
   <>
-    <Backdrop show={show} clicked={modalClosed} />
+    <Backdrop show={show} clicked={closeModal} />
     <div
-      className={classes.Modal}
+      className={styles.modal}
       style={{
         transform: show ? 'translateY(0)' : 'translateY(-100vh)',
         opacity: show ? '1' : '0'
       }}>
-      <span className={classes.crossIcon}>
-        <i className="fa fa-times" aria-hidden="true" onClick={modalClosed} />
+      <span className={styles.crossIcon}>
+        <i className="fa fa-times" aria-hidden="true" onClick={closeModal} />
       </span>
       {children}
     </div>
@@ -22,7 +22,7 @@ const Modal = ({ show, modalClosed, children }) => (
 
 Modal.propTypes = {
   show: PropTypes.bool.isRequired,
-  modalClosed: PropTypes.func.isRequired,
+  closeModal: PropTypes.func.isRequired,
   children: PropTypes.element.isRequired
 };
 
