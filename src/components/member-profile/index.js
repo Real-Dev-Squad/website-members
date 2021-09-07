21847c1c4474e2982beae062f2c6e759cfd9ed23
Lakshay Manchanda <lakshaymanchanda73@gmail.com>
2021-09-07 22:03:17 +0530

    Fixed ESLint issue

--- src/components/member-profile/index.js
+++ src/components/member-profile/index.js
@@ -9,6 +9,7 @@ import { motion } from 'framer-motion';
 import Modal from '@components/UI/modal';
 import ShowSkills from '@components/member-card/show-skills';
 import { useForm } from 'react-hook-form';
+import { isEmail, isDecimal } from 'validator';
 
 const renderBadgeImages = (badges) =>
   badges.map((badge) => (
@@ -75,7 +76,8 @@ const Profile = (props) => {
   const [showModal, setShowModal] = useState(false);
   const { register, handleSubmit, errors } = useForm();
 
-  const getMembersIntroURL = (RDSID) =>`https://api.realdevsquad.com/members/intro/${RDSID}`;
+  const getMembersIntroURL = (RDSID) =>
+    `https://api.realdevsquad.com/members/intro/${RDSID}`;
   const parameter = username;
 
   const onFormSubmit = (data) => {
@@ -99,7 +101,7 @@ const Profile = (props) => {
         alert(`Error!!!\n${error}`);
       })
       .finally(() => {
-        if (!confirm('Some error occurred!!! \nRETRY?')) {
+        if (!window.confirm('Some error occurred!!! \nRETRY?')) {
           setShowModal(false);
         }
       });
@@ -117,66 +119,66 @@ const Profile = (props) => {
       <form onSubmit={handleSubmit(onFormSubmit)}>
         <label className={classNames.tagWord} htmlFor="company">
           Company
+          <input
+            id="company"
+            className={classNames.inputBox}
+            type="text"
+            placeholder="Eg: Amazon"
+            name="company"
+            ref={register({
+              required: true,
+            })}
+          />
         </label>
-        <input
-          id="company"
-          className={classNames.inputBox}
-          type="text"
-          placeholder="Eg: Amazon"
-          name="company"
-          ref={register({
-            required: true,
-          })}
-        />
         {errors.company && (
           <p className={classNames.errorPrompt}>Company cannot be blank</p>
         )}
         <label className={classNames.tagWord} htmlFor="first_name">
           First Name
+          <input
+            id="first_name"
+            className={classNames.inputBox}
+            type="text"
+            placeholder="Eg: John"
+            name="first_name"
+            ref={register({
+              required: true,
+            })}
+          />
         </label>
-        <input
-          id="first_name"
-          className={classNames.inputBox}
-          type="text"
-          placeholder="Eg: John"
-          name="first_name"
-          ref={register({
-            required: true,
-          })}
-        />
         {errors.first_name && (
           <p className={classNames.errorPrompt}>First Name cannot be blank</p>
         )}
         <label className={classNames.tagWord} htmlFor="last_name">
           Last Name
+          <input
+            id="last_name"
+            className={classNames.inputBox}
+            type="text"
+            placeholder="Eg: Doe"
+            name="last_name"
+            ref={register({
+              required: true,
+            })}
+          />
         </label>
-        <input
-          id="last_name"
-          className={classNames.inputBox}
-          type="text"
-          placeholder="Eg: Doe"
-          name="last_name"
-          ref={register({
-            required: true,
-          })}
-        />
         {errors.last_name && (
           <p className={classNames.errorPrompt}>Last Name cannot be blank</p>
         )}
         <label className={classNames.tagWord} htmlFor="designation">
           Designation
+          <input
+            id="designation"
+            className={classNames.inputBox}
+            type="text"
+            placeholder="Eg: HR executive"
+            name="designation"
+            ref={register({
+              required: true,
+              minLength: 2,
+            })}
+          />
         </label>
-        <input
-          id="designation"
-          className={classNames.inputBox}
-          type="text"
-          placeholder="Eg: HR executive"
-          name="designation"
-          ref={register({
-            required: true,
-            minLength: 2,
-          })}
-        />
         {errors.designation?.type === 'required' && (
           <p className={classNames.errorPrompt}>Designation cannot be blank</p>
         )}
@@ -185,18 +187,18 @@ const Profile = (props) => {
         )}
         <label className={classNames.tagWord} htmlFor="reason">
           Reason
+          <input
+            id="reason"
+            className={classNames.inputBox}
+            type="text"
+            placeholder="Why you are interested in this candidate(Max 100 characters)"
+            name="reason"
+            ref={register({
+              required: true,
+              maxlength: 100,
+            })}
+          />
         </label>
-        <input
-          id="reason"
-          className={classNames.inputBox}
-          type="text"
-          placeholder="Why you are interested in this candidate(Max 100 characters)"
-          name="reason"
-          ref={register({
-            required: true,
-            maxlength: 100,
-          })}
-        />
         {errors.reason && (
           <p className={classNames.errorPrompt}>Reason cannot be blank</p>
         )}
@@ -205,18 +207,18 @@ const Profile = (props) => {
         )}
         <label className={classNames.tagWord} htmlFor="email">
           Email
+          <input
+            id="email"
+            className={classNames.inputBox}
+            type="email"
+            placeholder="Your Email address"
+            name="email"
+            ref={register({
+              required: true,
+              validate: (value) => isEmail(value) === true,
+            })}
+          />
         </label>
-        <input
-          id="email"
-          className={classNames.inputBox}
-          type="email"
-          placeholder="Your Email address"
-          name="email"
-          ref={register({
-            required: true,
-            validate: (value) => isEmail(value) === true,
-          })}
-        />
         {errors.email?.type === 'required' && (
           <p className={classNames.errorPrompt}>Email cannot be blank</p>
         )}
@@ -227,45 +229,45 @@ const Profile = (props) => {
         )}
         <label className={classNames.tagWord} htmlFor="currency">
           Currency
+          <div className={classNames.select}>
+            <select
+              id="currency"
+              type="select"
+              name="currency"
+              className={classNames.inputBoxCur}
+              ref={register({
+                validate: (value) => value !== '',
+              })}
+            >
+              <option value="">--Select--</option>
+              <option value="INR">Indian Rupee(INR)</option>
+              <option value="USD">United States Dollar(USD)</option>
+              <option value="EUR">Euro(EUR)</option>
+              <option value="GBP">Great Britain Pound(GBP)</option>
+              <option value="AUD">Australian Dollar(AUD)</option>
+              <option value="CAD">Canadian Dollar(CAD)</option>
+              <option value="SGD">Singapore Dollar(SGD)</option>
+              <option value="JPY">Japanese Yen(JPY)</option>
+            </select>
+            {errors.currency?.type === 'validate' && (
+              <p className={classNames.errorPrompt}>Provide a valid currency</p>
+            )}
+          </div>
         </label>
-        <div className={classNames.select}>
-          <select
-            id="currency"
-            type="select"
-            name="currency"
-            className={classNames.inputBoxCur}
-            ref={register({
-              validate: (value) => value !== '',
-            })}
-          >
-            <option value="">--Select--</option>
-            <option value="INR">Indian Rupee(INR)</option>
-            <option value="USD">United States Dollar(USD)</option>
-            <option value="EUR">Euro(EUR)</option>
-            <option value="GBP">Great Britain Pound(GBP)</option>
-            <option value="AUD">Australian Dollar(AUD)</option>
-            <option value="CAD">Canadian Dollar(CAD)</option>
-            <option value="SGD">Singapore Dollar(SGD)</option>
-            <option value="JPY">Japanese Yen(JPY)</option>
-          </select>
-          {errors.currency?.type === 'validate' && (
-            <p className={classNames.errorPrompt}>Provide a valid currency</p>
-          )}
-        </div>
         <label className={classNames.tagWord} htmlFor="package">
           Annual Package Offered
+          <input
+            id="package"
+            className={classNames.inputBox}
+            type="number"
+            placeholder="2000000"
+            name="package"
+            ref={register({
+              required: true,
+              validate: (value) => isDecimal(value) === true,
+            })}
+          />
         </label>
-        <input
-          id="package"
-          className={classNames.inputBox}
-          type="number"
-          placeholder="2000000"
-          name="package"
-          ref={register({
-            required: true,
-            validate: (value) => isDecimal(value) === true,
-          })}
-        />
         {errors.package && (
           <p className={classNames.errorPrompt}>
             Package offered cannot be blank
@@ -291,9 +293,10 @@ const Profile = (props) => {
         <Modal
           style={modalStyle}
           show={showModal}
-          children={children}
           closeModal={() => setShowModal(false)}
-        />
+        >
+          {children}
+        </Modal>
       )}
       <div className={(classNames.sidebar, classNames.column)}>
         <div className={classNames.memberDetails}>
