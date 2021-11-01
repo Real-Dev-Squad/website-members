42a18dfdb1fb2e0afd38001c98eb6b65738cbb9f
Prakash Choudhary <34452139+prakashchoudhary07@users.noreply.github.com>
2021-11-02 01:14:04 +0530

    Sync develop to main (#271)
    * Changed image size to 200px
    
    * Contribution image component using img of 40px
    
    * Remove unused const crop_face
    
    * Rename imageLink, refactor getImageLink function
    
    Co-authored-by: Ankush Dharkar <ankushdharkar@users.noreply.github.com>

--- src/constants/profile-image.js
+++ src/constants/profile-image.js
@@ -1,2 +1,4 @@
-export const MAX_WIDTH = 250;
-export const CROP_FACE_W250 = `c_crop,f_auto,g_face,h_400,w_400/c_scale,w_250`;
+export const WIDTH_200PX = 'w_200';
+export const WIDTH_40PX = 'w_40';
+export const HEIGHT_200PX = 'h_200';
+export const HEIGHT_40PX = 'h_40';
