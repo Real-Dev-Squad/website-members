f1084e0ecc0d07c9fe56d02dac2b4b807419d8e8
shashank <shashankkumar2023@gmail.com>
2022-01-24 16:12:03 +0530

    Removed comments and modified package.json

--- src/components/UI/backdrop/index.js
+++ src/components/UI/backdrop/index.js
@@ -1,11 +1,16 @@
-/* eslint-disable jsx-a11y/click-events-have-key-events */
-/* eslint-disable jsx-a11y/no-static-element-interactions */
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from '@components/UI/backdrop/backdrop.module.scss';
 
 const Backdrop = ({ show, clicked }) =>
-  show ? <div className={classNames.Backdrop} onClick={clicked} /> : null;
+  show ? (
+    <div
+      className={classNames.backdrop}
+      onClick={clicked}
+      onKeyDown={clicked}
+      aria-hidden
+    />
+  ) : null;
 
 Backdrop.propTypes = {
   show: PropTypes.bool.isRequired,
