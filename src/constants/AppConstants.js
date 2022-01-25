625051ad8f40d5e94b2ec9f28ee004e2c4277bbe
rohan09-raj <rajrohan1914@gmail.com>
2022-01-25 11:02:42 +0530

    fixed minor issues

--- src/constants/AppConstants.js
+++ src/constants/AppConstants.js
@@ -10,9 +10,6 @@ const PATHS = {
   CRYPTO: 'https://crypto.realdevsquad.com/',
   STATUS: 'https://status.realdevsquad.com/',
 };
-const SET_ERRORS = 'SET_ERRORS';
-const SET_MEMBERS = 'SET_MEMBERS';
-const HOST_NAME = 'members.realdevsquad.com';
 const LOGIN_URL =
   'https://github.com/login/oauth/authorize?client_id=23c78f66ab7964e5ef97';
 const USER_PROFILE_URL = 'https://my.realdevsquad.com/profile';
@@ -54,17 +51,14 @@ export {
   USER_DATA_URL,
   LOGIN_URL,
   PATHS,
-  SET_ERRORS,
-  SET_MEMBERS,
-  HOST_NAME,
   USER_PROFILE_URL,
   NAVMENU,
 };
 export const BRAND_NAME = 'Real Dev Squad';
 export const MEMBERS_TITLE = 'Members';
 export const NEW_MEMBERS_TITLE = 'New Members';
-export const SET_MEMBERS = 'SET_MEMBERS';
 export const SET_ERRORS = 'SET_ERRORS';
+export const SET_MEMBERS = 'SET_MEMBERS';
 export const HOST_NAME = 'members.realdevsquad.com';
 export const TIMEOUT = 1000;
 export const KEY_TAB = 9;
