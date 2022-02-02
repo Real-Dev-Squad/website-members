fabc7e81ffe50622298b6c0d403d77f0c8c7c9e9
RohitKuwar <rohit1kuwar@gmail.com>
2022-02-02 12:30:50 +0530

    added search members feature

---
+++ src/components/UI/search-box/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { searchMemberContext } from '@store/search-members/searchMembers-context';
+import classNames from './search-box.module.scss';
+
+const SearchBox = () => {
+  const { setSearchTerm } = searchMemberContext();
+  return (
+    <div className={classNames.searchContainer}>
+      <input
+        type="text"
+        placeholder="&#128269; search members here"
+        onChange={({ target: { value } }) => setSearchTerm(value)}
+        className={classNames.searchBox}
+      />
+    </div>
+  );
+};
+
+export default SearchBox;
---
+++ src/store/search-members/searchMembers-context.js
@@ -0,0 +1,25 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import { createContext, useState, useContext } from 'react';
+
+const SearchMemberContext = createContext();
+
+export const SearchMemberProvider = ({ children }) => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const initialSearchMemberContext = {
+    searchTerm,
+    setSearchTerm,
+  };
+  return (
+    <SearchMemberContext.Provider value={initialSearchMemberContext}>
+      {children}
+    </SearchMemberContext.Provider>
+  );
+};
+
+export const searchMemberContext = () => {
+  const context = useContext(SearchMemberContext);
+  if (!context)
+    throw new Error(`searchMemberContext context can only  
+        be used in a component wrapped with SearchMemberContext`);
+  return context;
+};
