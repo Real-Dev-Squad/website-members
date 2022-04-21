c12d1526112d03c307172676add95676ce21eaf4
shreya-mishra <shreyamishra040398@gmail.com>
2022-04-22 00:29:40 +0530

    designed search box and its functionality

---
+++ src/components/search-box/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import classNames from '@components/search-box/searchbox.module.scss';
+import { searchMemberContext } from '@store/search-members/searchMembers-context';
+
+const SearchBox = () => {
+  const { setSearchTerm } = searchMemberContext();
+
+  return (
+    <div className={classNames.searchbox_container}>
+      <input
+        className={classNames.searchbox}
+        type="text"
+        placeholder="Search by Skills, Accomplishments levels, Name"
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <button
+        type="button"
+        className={classNames.button}
+        // onClick={() => searchFunction(searchTerm)}
+      >
+        SEARCH
+      </button>
+    </div>
+  );
+};
+
+export default SearchBox;
