52a9c63793aaf6075acd7acd5f1dc183815d84de
Ishikabaid <ibishikabaid@gmail.com>
2022-08-04 20:05:37 +0530

    fixed members images

--- src/components/member-card/index.js
+++ src/components/member-card/index.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { useRouter } from 'next/router';
 import classNames from '@components/member-card/card.module.scss';
-import { motion } from 'framer-motion';
 import SocialMediaIcon from '@components/social-media-icon';
 import PropTypes from 'prop-types';
 import ShowSkills from '@components/member-card/show-skills';
@@ -46,7 +45,7 @@ const Card = ({ developerInfo, isOptionKey }) => {
       {dev && (
         <SuperUserOptions username={username} showSettings={showSettings} />
       )}
-      <motion.img
+      <img
         layoutId={username}
         src={isMember ? `${img_url}` : '/images/Avatar.png'}
         onError={brokenImageHandler}
