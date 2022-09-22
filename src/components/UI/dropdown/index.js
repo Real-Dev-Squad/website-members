26ba7b5fb89f84763fb90ab8d0f0eda17be77632
Saurabh <raisaurabh265@gmail.com>
2022-09-22 14:31:24 +0530

    dropdown component created with scss file

---
+++ src/components/UI/dropdown/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './dropdown.module.scss';
+
+const Dropdown = () => {
+  return (
+    <div className={styles.wrapper}>
+      <p>hello I am Dropdown</p>
+    </div>
+  );
+};
+
+export default Dropdown;
