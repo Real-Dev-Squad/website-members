6b0919aec208d8361370fad9066bd9b6d669d71c
Manas <manazharma@gmail.com>
2022-10-31 19:01:40 +0530

    fix: PR comments resolved

--- src/test/unit/components/member-profile/index.test.js
+++ src/test/unit/components/member-profile/index.test.js
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import Profile from '@components/member-profile';
 import { TaskContextProvider } from '@store/tasks/tasks-context';
+import { UserContextProvider } from '@store/user/user-context';
 
 const notaMember = {
   roles: {
@@ -14,20 +15,29 @@ const isaMember = {
     member: true,
   },
 };
+
+const initialUserContext = {
+  isSuperUser: true,
+};
+
 describe('Members Profile', () => {
   it('Should render member status properly', () => {
     render(
-      <TaskContextProvider>
-        <Profile membersData={notaMember} />
-      </TaskContextProvider>
+      <UserContextProvider value={initialUserContext}>
+        <TaskContextProvider>
+          <Profile membersData={notaMember} />
+        </TaskContextProvider>
+      </UserContextProvider>
     );
+
     let memberStatus = screen.getByText('User is not a Member');
     expect(memberStatus).toBeInTheDocument();
-
     render(
-      <TaskContextProvider>
-        <Profile membersData={isaMember} />
-      </TaskContextProvider>
+      <UserContextProvider value={initialUserContext}>
+        <TaskContextProvider>
+          <Profile membersData={isaMember} />
+        </TaskContextProvider>
+      </UserContextProvider>
     );
 
     memberStatus = screen.getByText('User is a Member');
@@ -36,10 +46,13 @@ describe('Members Profile', () => {
 
   it('Should render the info icon correctly', () => {
     render(
-      <TaskContextProvider>
-        <Profile membersData={notaMember} />
-      </TaskContextProvider>
+      <UserContextProvider value={initialUserContext}>
+        <TaskContextProvider>
+          <Profile membersData={notaMember} />
+        </TaskContextProvider>
+      </UserContextProvider>
     );
+
     const icon = screen.getByAltText('info icon');
     expect(icon).toBeDefined();
     expect(icon).toHaveAttribute('src', 'icons/info.png');
