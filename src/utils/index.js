05986aad4b2be29af861d057bb8d3c6c394cea03
Shubham Bajaj <shmbajaj32@gmail.com>
2022-11-15 23:57:30 +0530

    fix: getting auth url

--- src/utils/index.js
+++
@@ -1,7 +0,0 @@
-import { LOGIN_URL } from '@constants/AppConstants';
-
-export function getAuthUrl() {
-  const originURL = window.location?.href;
-  if (!originURL) return LOGIN_URL;
-  return `${LOGIN_URL}&state=${originURL}`;
-}
