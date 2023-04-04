9bd4bf24fd93354f8b64719f0a7c1274dc0f4dd5
Prakash Choudhary <34452139+prakashchoudhary07@users.noreply.github.com>
2023-04-04 20:49:53 +0530

    Fix superUser option key hold to show settings icon (#506)
    * add focusing the div on page load, to fix the bug where settings button is not shown when super user holds the option key
    
    * add keydow and up eventlistner on document and created keyboardHandler component in util
    
    * move keyboardHandler away from util and add setIsoption in deoendency array

--- src/store/keyboard/context.js
+++ src/store/keyboard/context.js
@@ -1,37 +1,18 @@
 import { createContext, useContext, useState } from 'react';
-import { ALT_KEY } from '@constants/AppConstants';
 
 const KeyboardContext = createContext();
-
-function isOptionKey(e) {
-  return e.key === ALT_KEY;
-}
 
 export const KeyboardProvider = ({ children }) => {
   const [isOptionKeyPressed, setIsOptionKeyPressed] = useState(false);
 
   const initialValue = {
     isOptionKeyPressed,
+    setIsOptionKeyPressed,
   };
 
   return (
     <KeyboardContext.Provider value={initialValue}>
-      <div
-        role="button"
-        tabIndex={0}
-        onKeyDown={(e) => {
-          if (isOptionKey(e)) {
-            setIsOptionKeyPressed(true);
-          }
-        }}
-        onKeyUp={(e) => {
-          if (isOptionKey(e)) {
-            setIsOptionKeyPressed(false);
-          }
-        }}
-      >
-        {children}
-      </div>
+      {children}
     </KeyboardContext.Provider>
   );
 };
