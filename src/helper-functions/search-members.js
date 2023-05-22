7ac26fcfbe3b163be970389aac9fb86079d91de1
ritikjaiswal75 <ritik12103032000@gmail.com>
2023-05-22 09:42:28 +0530

    remove changes from .env.development

--- src/helper-functions/search-members.js
+++ src/helper-functions/search-members.js
@@ -2,9 +2,9 @@ const searchMembers = (membersArr, searchTerm) => {
   return membersArr.filter(
     (member) =>
       member.first_name
-        .toLowerCase()
+        ?.toLowerCase()
         .includes(searchTerm.toLocaleLowerCase()) ||
-      member.last_name.toLowerCase().includes(searchTerm.toLocaleLowerCase())
+      member.last_name?.toLowerCase().includes(searchTerm.toLocaleLowerCase())
   );
 };
 
