da145ac2c1335cc283c5f6b1354f0ef010c09be4
DevSoni2022 <121338813+DevSoni2022@users.noreply.github.com>
2023-10-17 00:13:58 +0530

    Social url issue fix (#563)
    * blnk commit
    
    * social url containing https fixed
    
    * CheckId to checkId

--- src/components/social-media-icon/index.js
+++ src/components/social-media-icon/index.js
@@ -6,6 +6,14 @@ import classNames from '@components/social-media-icon/social-media-icon.module.s
 const SocialMediaIcon = (props) => {
   const { id, type } = props;
 
+  let checkId = id;
+
+  if (checkId.includes('https') || checkId.includes('http')) {
+    checkId = checkId.split('/').pop();
+  } else {
+    checkId = id;
+  }
+
   const onClick = (e) => {
     e.stopPropagation();
   };
@@ -17,7 +25,7 @@ const SocialMediaIcon = (props) => {
       target="_blank"
       rel="noreferrer"
       tabIndex="0"
-      href={`${iconMapper[type].href}/${[id]}`}
+      href={`${iconMapper[type].href}/${[checkId]}`}
     >
       <img
         className={classNames.iconImage}
