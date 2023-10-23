a65bc3eda69affe186993e765465ba27a395ee84
Shubham Jain <shubhamjain2796@gmail.com>
2023-10-23 18:03:06 +0530

    fixed error msg for falsy start date (#565)

--- src/helper-functions/taskProgress.js
+++ src/helper-functions/taskProgress.js
@@ -1,8 +1,13 @@
 import { timeWas } from '@helper-functions/time-was';
 
-const percentageofDaysRemaining = (startedOn, endsOn, completedDate) => {
-  const startDate = timeWas(startedOn * 1000, true, endsOn * 1000);
-  const endDate = timeWas(endsOn * 1000, true, startDate * 1000);
+const percentageofDaysRemaining = (
+  startedOn,
+  endsOn,
+  completedDate,
+  devUser
+) => {
+  const startDate = timeWas(startedOn * 1000, true, endsOn * 1000, devUser);
+  const endDate = timeWas(endsOn * 1000, true, startDate * 1000, devUser);
   const date1 = new Date(startDate);
   const date2 = new Date(endDate);
   const diffTime = Math.abs(date2 - date1);
